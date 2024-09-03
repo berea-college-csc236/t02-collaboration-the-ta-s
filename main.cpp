787d0be3cd5708b4c63b765c0ee29cb9d78761d8
ayeleworkm <ayeleworkm@berea.edu>
2024-09-03 19:51:09 -0400

    Update main.cpp
    forgot to change the rubber duck

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main() {
     cin >> ducknum;
 
     for (int i = 0; i < ducknum; i++) { 
-        cout << "                __     " << endl; // TODO: student 1 may change this line and may add additional lines immediately above
+        cout << "                ***     " << endl; // TODO: student 1 may change this line and may add additional lines immediately above
         cout << "              <(o )___-" << endl; // student 3(1) may change this line and add additional lines between this line and immediately below
         cout << "               ( .__> /" << endl; // student 3(2) may change this line
         cout << "                `----' " << endl; // TODO student 2 may change this line and may add lines below
